0c1df1c91e7ae76d6c2a915bcdffd730813b104e
dikuchan <dikuchan@yahoo.com>
2020-12-11 00:24:17 +0300

    Add ready check

--- clusterizer/main.py
+++ clusterizer/main.py
@@ -15,16 +15,34 @@ def tags():
     bottle.response.set_header('Content-Type', 'application/json')
     bottle.response.set_header('Cache-Control', 'no-cache')
 
-    logging.info('Received GET')
+    logging.info('Received GET on tags')
+
+    if not clusterizer.processed:
+        return bottle.HTTPResponse(status=403,
+                                   body=json.dumps({}))
 
     return json.dumps({
         'clusters': clusterizer.get_tags()
     })
 
 
+@post('/api/ready')
+def ready():
+    logging.info('Scraper is ready. Starting analysis')
+
+    if clusterizer.processed:
+        logging.info('Data was clustered already')
+        return
+
+    clusterizer.process_data(n_tweets=arguments.tweets)
+    clusterizer.cluster_data(n_clusters=arguments.clusters)
+
+    logging.info('Processed data. Ready to start a scraper...')
+
+
 @post('/api/recommend')
 def recommend():
-    logging.info('Received POST')
+    logging.info('Received POST on recommend')
 
     try:
         data = bottle.request.json()
@@ -39,6 +57,10 @@ def recommend():
     bottle.response.set_header('Content-Type', 'application/json')
     bottle.response.set_header('Cache-Control', 'no-cache')
 
+    if not clusterizer.processed:
+        return bottle.HTTPResponse(status=403,
+                                   body=json.dumps({}))
+
     return json.dumps({
         'users': [clusterizer.get_recommendations(tag['n'])
                   for tag in data['tags']]})
@@ -48,7 +70,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Cluster Twitter users')
     parser.add_argument('es', help='Address of an Elasticsearch instance')
     parser.add_argument('server', help='IP and port of a scraper server')
-    parser.add_argument('--tweets', help='Number of tweets to extract', type=int, default=100_000)
+    parser.add_argument('--tweets', help='Number of tweets to extract', type=int, default=1_000_000)
     parser.add_argument('--clusters', help='Number of clusters to form', type=int, default=24)
 
     arguments = parser.parse_args()
@@ -57,9 +79,5 @@ if __name__ == '__main__':
     port = arguments.server.split(':')[1]
 
     clusterizer = Clusterizer(es='http://' + arguments.es)
-    clusterizer.process_data(n_tweets=arguments.tweets)
-    clusterizer.cluster_data(n_clusters=arguments.clusters)
-
-    logging.info('Processed data. Ready to start a scraper...')
 
     bottle.run(host=host, port=port)
