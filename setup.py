d1db5d3c9f58339809a7bbf7c484b4f580e3e8cf
dikuchan <dikuchan@yahoo.com>
2020-12-11 15:57:32 +0300

    Add global setup script

--- setup.py
+++ setup.py
@@ -1,81 +1,40 @@
 #!/usr/bin/env python
 
-import logging
 import argparse
-
-import os
-import grp
-import getpass
 import subprocess
-import re
 
-configs = [config
-           for config in os.listdir('../istio')
-           if os.path.isfile(config)]
-
-
-def run(command) -> (str, str, int):
-    result = subprocess.run(command,
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE)
-    output = result.stdout.decode('utf-8')
-    error = result.stderr.decode('utf-8')
-    code = result.returncode
-
-    return output, error, code
-
-
-def is_root() -> bool:
-    return os.getuid() != 0
-
-
-def in_group(group) -> bool:
-    user = getpass.getuser()
-    groups = [g.gr_name
-              for g in grp.getgrall()
-              if user in g.gr_mem]
-
-    return group in groups
-
-
-def clean_up():
-    logging.info('Removing Kubernetes units')
-    for config in configs:
-        run(['kubectl', 'delete', '-f', config])
-
-
-def check_istio(cloud):
-    logging.info('Checking Istio installation')
-    status, _, _ = run(['kubectl', 'get', 'namespaces'])
-    if not bool(re.search(r'istio-system', status)):
-        logging.info('Istio is not installed on cluster')
-        logging.info('Installing Istio')
-        _, error, _ = run(['command', '-v', 'istioctl'])
-        run(['istioctl', 'install', '--set', 'profile=demo', '-y'])
-
-
-def add_istio_label():
-    status, _, code = run(['kubectl', 'get', 'namespace', 'default', '--show-labels'])
-    if not bool(re.search(r'istio-injection=enabled', status)):
-        run(['kubectl', 'label', 'namespace', 'default', 'istio-injection=enabled'])
-
-
-def upload_units():
-    logging.info('Uploading Kubernetes units')
-    for config in configs:
-        run(['kubectl', 'apply', '-f', config])
-
-
-def check_minikube():
-    status, _, _ = run(['minikube', 'status'])
-    logging.info('Checked status of Minikube')
-    if not bool(re.search(r'(host|kubelet|apiserver|kubeconfig): Running', status)):
-        logging.warning('Minikube is not started. Launching...')
-        status, error, _ = run(['minikube', 'start'])
-        if error != '':
-            logging.error('Cannot start Minikube')
-            exit(1)
-
+global pods
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser(description='Setup Twittify Application')
+
+    parser.add_argument('--istio', '-i', help='Start Istio cluster and upload Kubernetes units',
+                        nargs='?', choices=['local', 'cloud'])
+    parser.add_argument('--cleanup', '-c', help='Clean up Kubernetes units', action='store_true')
+    parser.add_argument('--restart', '-r', help='Restart pods', required=False, nargs='*')
+    parser.add_argument('--run', help='Run Minikube tunnel', action='store_true')
+    parser.add_argument('--address', '-a', help='Discover address of a cluster', action='store_true')
+
+    arguments = parser.parse_args()
+
+    if arguments.istio is not None:
+        if arguments.istio == 'local':
+            subprocess.run(['./scripts/istio-setup-local.sh'])
+        if arguments.istio == 'cloud':
+            subprocess.run(['./scripts/istio-setup-gcloud.sh'])
+
+    if arguments.cleanup:
+        subprocess.run(['./scripts/cleanup.sh'])
+
+    if arguments.restart is not None:
+        pods = ['client', 'scraper', 'clusterizer']
+        if arguments.restart != [] and arguments.restart != ['all']:
+            pods = arguments.restart
+        for pod in pods:
+            subprocess.run(['kubectl', 'rollout', 'restart', 'deployment/twittify' + pod])
+
+    if arguments.run:
+        subprocess.run(['./scripts/run-tunnel.sh'])
+
+    if arguments.address:
+        subprocess.run(['./scripts/get-address.sh'])
