16653559bf8961b2ee60fa52103fb6591ad60b25
dikuchan <dikuchan@yahoo.com>
2020-12-12 03:12:30 +0300

    Reorganize Clusterizer

--- clusterizer/main.py
+++ clusterizer/main.py
@@ -4,82 +4,71 @@ import bottle
 import argparse
 
 from bottle import get, post
-
-from clustering import Clusterizer
-
-logging.basicConfig(level=logging.INFO)
+from lib import Clusterizer
 
 
 @get('/api/tags')
 def tags():
     bottle.response.set_header('Content-Type', 'application/json')
-    bottle.response.set_header('Cache-Control', 'no-cache')
-
-    logging.info('Received GET on tags')
 
     if not clusterizer.processed:
-        return bottle.HTTPResponse(status=403,
-                                   body=json.dumps({}))
+        return bottle.HTTPResponse(status=403)
 
-    return json.dumps({
-        'clusters': clusterizer.get_tags()
-    })
+    return json.dumps({'clusters': clusterizer.tags()})
 
 
 @post('/api/ready')
 def ready():
-    logging.info('Scraper is ready. Starting analysis')
-
     if clusterizer.processed:
-        logging.info('Data was clustered already')
+        logging.info('Clusters already defined')
         return
 
-    clusterizer.process_data(n_tweets=arguments.tweets)
-    clusterizer.cluster_data(n_clusters=arguments.clusters)
+    logging.info('Scraper is ready')
+    logging.info('Starting analysis')
 
-    logging.info('Processed data. Ready to start a scraper...')
+    clusterizer.process(n_tweets=arguments.tweets)
+    clusterizer.cluster(n_clusters=arguments.clusters)
+
+    logging.info('Defined clusters')
+    logging.info('Clusterizer is launching')
 
 
 @post('/api/recommend')
 def recommend():
-    logging.info('Received POST on recommend')
+    if not clusterizer.processed:
+        return bottle.HTTPResponse(status=403)
 
     try:
         data = bottle.request.json
-    except ValueError:
-        logging.error('Cannot parse input data of `recommend`')
+    except Exception as e:
+        logging.error(e)
         return
 
     if data is None:
-        logging.error('Data of `recommend` is empty')
+        logging.error('User data is empty')
         return
 
     bottle.response.set_header('Content-Type', 'application/json')
-    bottle.response.set_header('Cache-Control', 'no-cache')
 
-    if not clusterizer.processed:
-        return bottle.HTTPResponse(status=403,
-                                   body=json.dumps({}))
-
-    users = []
-    for tag in data['tags']:
-        users.extend(clusterizer.get_recommendations(tag['n']))
-
-    return json.dumps({'users': users})
+    return json.dumps({'users': [
+        user for tag in data['tags']
+        for user in clusterizer.recommend(tag['n'])
+    ]})
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Cluster Twitter users')
-    parser.add_argument('es', help='Address of an Elasticsearch instance')
-    parser.add_argument('server', help='IP and port of a scraper server')
-    parser.add_argument('--tweets', help='Number of tweets to extract', type=int, default=1_500_000)
+
+    parser.add_argument('--elastic', help='Address of an Elasticsearch instance', default='localhost:9200')
+    parser.add_argument('--server', help='IP and port of a Clusterizer server', default='0.0.0.0:8787')
+    parser.add_argument('--tweets', help='Number of tweets to extract', type=int, default=2_000_000)
     parser.add_argument('--clusters', help='Number of clusters to form', type=int, default=18)
 
     arguments = parser.parse_args()
 
+    clusterizer = Clusterizer(elastic=arguments.elastic)
+
     host = arguments.server.split(':')[0]
     port = arguments.server.split(':')[1]
-
-    clusterizer = Clusterizer(es='http://' + arguments.es)
 
     bottle.run(host=host, port=port)
