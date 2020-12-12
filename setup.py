b57cdfbd62e987ce889f6bf051451c3c3a95ca2e
dikuchan <dikuchan@yahoo.com>
2020-12-12 15:17:40 +0300

    Add auto-update option to setup script

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ import argparse
 import subprocess
 
 global pods
+global images
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Setup Twittify Application')
@@ -15,6 +16,7 @@ if __name__ == '__main__':
     parser.add_argument('--run', help='Run Minikube tunnel', action='store_true')
     parser.add_argument('--address', '-a', help='Discover address of a cluster', action='store_true')
     parser.add_argument('--test', '-t', help='Test Scraper and Clusterizer locally', action='store_true')
+    parser.add_argument('--update', '-u', help='Update Docker images on Docker Hub', nargs='*')
 
     arguments = parser.parse_args()
 
@@ -30,13 +32,20 @@ if __name__ == '__main__':
         if arguments.restart != [] and arguments.restart != ['all']:
             pods = arguments.restart
         for pod in pods:
-            subprocess.run(['kubectl', 'rollout', 'restart', 'deployment/twittify' + pod])
+            subprocess.run(['kubectl', 'rollout', 'restart', f'deployment/twittify-{pod}'])
     elif arguments.run:
         subprocess.run(['./scripts/run-tunnel.sh'])
     elif arguments.address:
         subprocess.run(['./scripts/get-address.sh'])
     elif arguments.test:
         subprocess.run(['./scripts/local-setup.py'])
+    elif arguments.update is not None:
+        images = ['clusterizer', 'scraper']
+        if arguments.update != [] and arguments.update != ['all']:
+            images = arguments.update
+        for image in images:
+            subprocess.run(['docker', 'build', '-t', f'dikuchan/twittify-{image}', f'./{image}'])
+            subprocess.run(['docker', 'push', f'dikuchan/twittify-{image}'])
     else:
         print('Consider checking README.md')
         parser.print_help()
